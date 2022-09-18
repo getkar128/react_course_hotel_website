3ff65787704c1b4ff6002dd4ec780b2707484502
Karthik <getkar128@gmail.com>
2022-09-18 14:45:06 +0300

    Functional Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent';
-import {DISHES} from './shared/dishes'
 import { Component } from 'react';
 
 class App extends Component {
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,18 +1,14 @@
-import React, {Component} from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { format } from 'date-fns'
-class DishDetail extends Component {
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-    constructor(props) {
-        super(props);
-        
-    }
 
-    renderDish(dish) {
+
+
+    function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <Card>
-                    <CardImg width="100%" object src={dish.image} alt={dish.name}></CardImg>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
@@ -27,14 +23,9 @@ class DishDetail extends Component {
     }
 
 
-    renderComments(dish) {
+    function RenderComments({dish}) {
         if (dish != null && dish.comments != null) {
             const comments = dish.comments.map((comment)=>{
-                // const date = format(comment.date.slice(0, 10),'MMMM dd, yyyy')
-                const date = comment.date.slice(0, 10).split('-')
-                console.log(date);
-                const newDate = format(new Date(date[0], date[1]-1, date[2]), 'MMM dd, yyyy')
-                console.log(newDate);
                 return (
                 <div key={comment.id} className='m-1'>
                     <li className="m-2">{comment.comment}</li>
@@ -58,24 +49,25 @@ class DishDetail extends Component {
             )
         }
     }
-    render() {
-        
+
+    const DishDetail = (props) => {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                        <RenderDish dish={props.dish} />
                     </div>
                 
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish)}
+                        <RenderComments dish={props.dish} />
                     </div>
                 </div>
             </div>
         )
+    }
        
 
-    }
-}
+    
+
 
 export default DishDetail
