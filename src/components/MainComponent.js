d02cdfae51148a21f9580653d5f0ee31564c46db
Karthik <getkar128@gmail.com>
2022-09-19 16:40:10 +0300

    Added About Component in second exercise

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect} from 'react-router-dom';
+import About from './AboutComponent';
 
 class Main extends Component {
   constructor(props){
@@ -50,6 +51,7 @@ class Main extends Component {
         <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/>}/>
         <Route path='/menu/:dishId' component={DishWithId} />
         <Route exact path='/contactus' component={Contact} />
+        <Route exact path='/aboutus' component={()=> <About leaders={this.state.leaders}/>} />
         <Redirect to='/home'/>
       </Switch>
       <Footer />
