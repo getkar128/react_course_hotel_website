cfef3c04668bfcf6ea8da9814427b22620219ebc
Karthik <getkar128@gmail.com>
2022-10-01 17:22:05 +0300

    fetch post comments

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -10,14 +10,12 @@ export const Comments = (state = {
             return {...state, isLoading: false, errMess: null, comments: action.payload};
 
         case ActionTypes.COMMENTS_FAILED:
-        return {...state, isLoading: false, errMess: action.payload, comments: []};
+            return {...state, isLoading: false, errMess: action.payload, comments: []};
 
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
-            comment.id = state.comments.length;
-            comment.date = new Date().toISOString();
             return {...state, comments: state.comments.concat(comment)};
-            
+
         default:
             return state;
     }
